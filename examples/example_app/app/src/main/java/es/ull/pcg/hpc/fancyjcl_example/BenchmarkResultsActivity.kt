0f7f191e39db685adac26a9f943486a0890616d0
ogomez <oscar2712@gmail.com>
2022-05-02 20:55:35 +0100

    Write benchmarks to CSV

--- examples/example_app/app/src/main/java/es/ull/pcg/hpc/fancyjcl_example/BenchmarkResultsActivity.kt
+++ examples/example_app/app/src/main/java/es/ull/pcg/hpc/fancyjcl_example/BenchmarkResultsActivity.kt
@@ -5,12 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import com.jraska.console.Console
 import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.io.File
 import java.lang.String
 import java.nio.ByteBuffer
 
 class BenchmarkResultsActivity : AppCompatActivity() {
+    private var file: File
     private val JAVA_N_EXECUTIONS = 3
     private val JCL_N_EXECUTIONS = 15
+
+    init {
+        val path = MainActivity.ctx.getExternalFilesDir(null)
+        file = File(path, "benchmarks.csv")
+        file.delete()
+        file.createNewFile()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_benchmark_results)
@@ -18,6 +28,7 @@ class BenchmarkResultsActivity : AppCompatActivity() {
         var resolutionIndices = intent.getIntegerArrayListExtra("resolutionIndexArray")
         MainActivity.scopeBackground.launch {
             Console.clear()
+            writeCSVHeader()
             for (filterIdx in filterIndices!!) {
                 val name = resources.getStringArray(R.array.filter_names)[filterIdx]
                 Console.writeLine(" ------- Executing benchmarks for $name")
@@ -29,10 +40,20 @@ class BenchmarkResultsActivity : AppCompatActivity() {
                     Timber.d("   - Resolution: $description")
                     val w = resources.getIntArray(R.array.resolutions_width)[resolutionIdx]
                     val h = resources.getIntArray(R.array.resolutions_height)[resolutionIdx]
+                    val resolutionName =
+                        resources.getStringArray(R.array.resolution_names)[resolutionIdx]
+                    file.appendText("$name,$resolutionName,")
                     runBenchmark(filterIdx, w, h)
+                    file.appendText("\n")
                 }
             }
         }
+    }
+
+    private fun writeCSVHeader() {
+        val header = "Filter,Resolution,Java,FancyJCL"
+        file.appendText(header)
+        file.appendText("\n")
     }
 
     private fun runBenchmark(filterIdx: Int, w: Int, h: Int) {
@@ -45,10 +66,12 @@ class BenchmarkResultsActivity : AppCompatActivity() {
         val filter = MainActivity.getFilterByIndex(filterIdx)
         val javaTime = filter.benchmarkJava(inputJava, outputJava, w, h, JAVA_N_EXECUTIONS)
         val javaTimeStr = String.format("%.2f", javaTime)
+        file.appendText("$javaTimeStr,")
         Console.writeLine("         Java: $javaTimeStr milliseconds")
         Timber.d("         Java: $javaTimeStr milliseconds")
         val jclTime = filter.benchmarkFancyJCL(inputJCL, outputJCL, w, h, JCL_N_EXECUTIONS)
         val jclTimeStr = String.format("%.2f", jclTime)
+        file.appendText(jclTimeStr)
         Console.writeLine("         FancyJCL: $jclTimeStr milliseconds")
         Timber.d("         FancyJCL: $jclTimeStr milliseconds")
     }
